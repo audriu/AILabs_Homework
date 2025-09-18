3dba1a41655b420bb6f4f6d5cac31762b0761979
Audrius Molis <vienas00@gmail.com>
2025-09-18 10:25:00 +0300

    Refactor chat agent

--- chat_agent.py
+++ chat_agent.py
@@ -1,21 +1,17 @@
 from smolagents import ToolCallingAgent
-from smolagents.models import LiteLLMModel
-from rag_tool import rag_tool
 
-class ChatAgent:
-    def __init__(self, model="llama3.1:8b"):
-        self.name = "Tech support question"
-        self.model = LiteLLMModel(model_id=f"ollama/{model}")
-        self.agent = ToolCallingAgent(
-            tools=[rag_tool],
-            model=self.model
+
+class ChatAgent(ToolCallingAgent):
+    def __init__(self, model, tools=[]):
+        super().__init__(
+            tools=tools,
+            model=model
         )
-        self.memory = self.agent.memory
 
     def chat(self, message: str) -> str:
         """Main chat function with smolagents"""
         try:
-            response = self.agent.run(message)
+            response = self.run(message)
             return str(response)
         except Exception as e:
             return f"Error: {str(e)}"
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+from smolagents import AgentMemory, LiteLLMModel
+
 from chat_agent import ChatAgent
+from rag_tool import rag_tool
 
 app = Flask(__name__)
-agent = ChatAgent()
+model = LiteLLMModel(model_id=f"ollama/llama3.1:8b")
+agent = ChatAgent(model=model, tools=[rag_tool])
 
 
 @app.route('/')
@@ -14,6 +18,12 @@ def index():
 def chat():
     message = request.json.get('message')
     response = agent.chat(message)
+    print("----------------------------------------------0")
+    print(response)
+    print("----------------------------------------------1")
+    memory: AgentMemory = agent.memory
+    print(memory.get_full_steps())
+    print("----------------------------------------------2")
     return jsonify({'response': response})
 
 
